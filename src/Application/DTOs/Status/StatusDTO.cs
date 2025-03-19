766773b633d0c9255324100c5d2bb1c2c6485eed
lucasb-silva <lucasbeserrasilva18@gmail.com>
2025-03-18 21:52:26 -0300

    Criando as classes DTO

---
+++ src/Application/DTOs/Status/StatusDTO.cs
@@ -0,0 +1,8 @@
+namespace ClinAgenda.src.Application.DTOs.Status
+{
+    public class StatusDTO
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
---
+++ src/Application/DTOs/Status/StatusInsertDTO.cs
@@ -0,0 +1,7 @@
+namespace ClinAgenda.src.Application.DTOs.Status
+{
+    public class StatusInsertDTO
+    {
+        public required string Name { get; set; }
+    }
+}
